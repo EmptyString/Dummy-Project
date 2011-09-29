a23385f3400588b6790d5c9128caab4f9c301dd5
Philipp Werner <philipp@LINUX-DEBIAN.fritz.box>
2011-09-29 19:16:18 +0200

    some pretty amazing things have been done

--- hello.cpp
+++ hello.cpp
@@ -4,11 +4,16 @@
  */
 #include <iostream>
 
+#include "uberness.hpp"
+
 int main(int argc, char** argv)
 {
 	std::cout << "Hello Guys" << std::endl;
 
 	// do some awesome stuff
+
+	AwesomeClass ac;
+	ac.beAwesome();
 
 	return 0;
 }
