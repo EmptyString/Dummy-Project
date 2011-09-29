21b378dcbdce2ff8128bf41902e425ddeba65a24
Philipp Werner <philipp@LINUX-DEBIAN.fritz.box>
2011-09-29 20:16:13 +0200

    updated exp-branch

---
+++ uberness.hpp
@@ -0,0 +1,23 @@
+/*
+ * \name: uberness.hpp
+ * \description: answer to everything
+ */
+
+class AwesomeClass
+{
+private:
+	void _doCoolThinngs()
+	{
+		// be cool
+	}
+
+public:
+	AwesomeClass() 
+	{
+	}
+
+	void beAwesome() 
+	{
+		return;
+	}
+};
